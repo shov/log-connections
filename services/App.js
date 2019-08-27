d874a143d572765279b2977a14aa5646951dd55a
Alexandr Shevchenko <ls.shov@gmail.com>
2019-08-27 12:07:20 +0300

    Added SSL support

--- services/App.js
+++ services/App.js
@@ -32,11 +32,32 @@ class App {
    */
   start() {
     const expressApp = (require('express'))()
+    const http = require('http')
+    const https = require('https')
+    const fs = require('fs')
 
     expressApp.use(this._router)
 
-    expressApp.listen(this._config.port, () => {
-      this._logger.log(`Start listening on ${this._config.port}`)
+    if (process.env.SSL && 'true' === process.env.SSL) {
+
+      const DEFAULT_KEY = '/certs/privatekey.pem'
+      const DEFAULT_CERT = '/certs/certificate.pem'
+
+      const privateKey = fs.readFileSync(APP_PATH + (process.env.SSL_KEY || DEFAULT_KEY))
+      const certificate = fs.readFileSync(APP_PATH + (process.env.SSL_CERT || DEFAULT_CERT))
+
+      https.createServer({
+        key: privateKey,
+        cert: certificate
+      }, expressApp).listen(this._config.port, () => {
+        this._logger.log(`[HTTPS] Start listening on ${this._config.port}`)
+      })
+
+      return
+    }
+
+    http.createServer(expressApp).listen(this._config.port, () => {
+      this._logger.log(`[HTTP] Start listening on ${this._config.port}`)
     })
   }
 }
